b716f3c3a99e3c35db5f98bf2cb3288eeeb23308
SergioFi666 <sergio.figueroa2304@gmail.com>
2020-01-08 17:44:33 +0100

    Style Tablas

--- index.js
+++ index.js
@@ -4,18 +4,43 @@ function loadGame() {
     var marcadorBlanc = 2;
     var marcadorNegre = 2;
     var mida = document.getElementById('mida').value;
+    var width;
+    var height;
     mida = parseInt(mida);
+
+    switch(mida){
+
+        case 8:
+            width = 558;
+            height = 558;
+            break;
+        case 10:
+            width = 520;
+            height = 520;
+            break;
+        case 16:
+            width = 640;
+            height = 640;
+            break;
+    }
 
     console.log(jugadorNegre);
     console.log(jugadorBlanc);
     console.log(mida);
 
-    document.getElementById('tablero').style.display = 'block';
+    document.getElementById('espai').style.display = 'none';
+    document.getElementById('tablero').style.display = 'flex';
+    document.getElementById('tablero').style.flexDirection = 'row'; 
     document.getElementById('info').style.display = 'block';
     document.getElementById('container-form').style.display = 'none';
 
     document.getElementById('nom-blanc').innerHTML = jugadorBlanc;
     document.getElementById('nom-negre').innerHTML = jugadorNegre;
+
+    var tbl = document.getElementById('taula');
+    var stl = document.createAttribute('style');
+    stl.value = "width: " + width + "px; height: " + height + "px;";
+    tbl.setAttributeNode(stl);
 
     for (let i = 0; i < mida; i++) {
         var tr = document.createElement("tr");
